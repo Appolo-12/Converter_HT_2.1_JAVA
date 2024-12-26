6813fcda40c4f7e0826f637387a5867e49e5b4d9
Appolo-13 <rayl1x.gta@gmail.com>
2024-12-26 19:34:57 +0100

    Add conversion from mph to km/h and vice versa.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This project will convert km/h to mph and vice versa.");
 
-        double mph = 1;
+        double kmH = 90;
+        double mph = 60;
+        double convertedKilometers = convertKmToMPH(kmH);
         double convertedMiles = convertMPHToKm(mph);
+
+        System.out.println("Kilometers without conversion to miles: " + kmH + "\n" +
+                "Kilometers with conversion to miles: " + convertedKilometers);
 
         System.out.println("Miles without conversion to kilometers: " + mph + "\n" +
                 "Miles with conversion to kilometers: " + convertedMiles);
+    }
+
+    private static double convertKmToMPH(double kmH) {
+        return kmH * CONVERT_KMH;
     }
 
     private static double convertMPHToKm(double mph) {
